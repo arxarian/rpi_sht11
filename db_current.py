68c263b142f90fef6db50a3652d51049272a9ec0
ar <arxarian@gmail.com>
2020-04-25 19:01:14 +0200

    replace CRLF by LF only
    Signed-off-by: ar <arxarian@gmail.com>

---
+++ db_current.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import mysql.connector
+
+class DB_CurrentMeasurement:
+    def __init__(self):
+        self.table = "currentmeasurement"
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="measurer",
+            passwd="thefifthfloor",
+            database="measurements"
+        )
+        self.cursor = self.db.cursor()
+        self.__CreateTableIfNotExists()
+
+    def __CreateTableIfNotExists(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS " + str(self.table) + " (id INT AUTO_INCREMENT PRIMARY KEY, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, temperature DECIMAL(16,14), humidity DECIMAL(16,14))") 
+
+
+    def Update(self, temperature, humidity):
+        sql = "DELETE FROM " + self.table
+        self.cursor.execute(sql)
+    
+        sql = "INSERT INTO " + self.table + " (temperature, humidity) VALUES (%s, %s)"
+        val = (temperature, humidity)
+        self.cursor.execute(sql, val)
+
+        self.db.commit()
+
+    def PrintAll(self):
+        self.cursor.execute("SELECT * FROM " + self.table)
+
+        myresult = self.cursor.fetchall()
+
+        for x in myresult:
+            print(x)
---
+++ db_measurements.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import mysql.connector
+
+class DB_Measurements:
+    def __init__(self):
+        self.table = "measurements"
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="measurer",
+            passwd="thefifthfloor",
+            database="measurements"
+        )
+        self.cursor = self.db.cursor()
+        self.__CreateTableIfNotExists()
+
+    def __CreateTableIfNotExists(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS " + str(self.table) + " (id INT AUTO_INCREMENT PRIMARY KEY, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, temperature DECIMAL(16,14), humidity DECIMAL(16,14))") 
+
+    def Add(self, temperature, humidity):
+        sql = "INSERT INTO " + str(self.table) + " (temperature, humidity) VALUES (%s, %s)"
+        val = (temperature, humidity)
+        self.cursor.execute(sql, val)
+
+        self.db.commit()
+
+    def PrintAll(self):
+        self.cursor.execute("SELECT * FROM " + str(self.table))
+
+        myresult = self.cursor.fetchall()
+
+        for x in myresult:
+            print(x)
