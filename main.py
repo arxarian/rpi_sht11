1076a0cdbf66571e521cc58198862f8d57c48f4c
ar <arxarian@gmail.com>
2020-04-25 19:03:58 +0200

    add primitive timer-based measurement scheduler
    Signed-off-by: ar <arxarian@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
 
 import sys
+import time
+from datetime import datetime
 
-import database
 import sensors
+import db_current
+import db_measurements
 
-from threading import Timer
+sht11 = sensors.SHT11()
+currentMeasurement = db_current.DB_CurrentMeasurement()
+measurements = db_measurements.DB_Measurements()
 
-SHT11 = sensors.SHT11()
-measurement = SHT11.measure()
+totalTemperature = 0
+totalHumidity = 0
+totalMeasurements = 0
+averageInterval = 15 # seconds
+lastTimestamp = datetime.timestamp(datetime.now())
 
-DB = database.Database()
-DB.AddMeasurement(measurement[0], measurement[1])
-DB.PrintAll()
+while True:
+    measurement = sht11.measure()
+    currentMeasurement.Update(measurement[0], measurement[1])
+
+    timestamp = datetime.timestamp(datetime.now())
+    totalTemperature += measurement[0]
+    totalHumidity += measurement[1]
+    totalMeasurements += 1
+
+    print(timestamp - lastTimestamp)
+
+    if (timestamp - lastTimestamp) > averageInterval:
+        averageTemperature = totalTemperature  / totalMeasurements
+        averageHumidity = totalHumidity  / totalMeasurements
+        print("average temperature: " + str(averageTemperature) + " °C")
+        print("average humidity: " + str(averageHumidity) + " %")
+
+        measurements.Add(measurement[0], measurement[1])
+        totalTemperature = 0
+        totalHumidity = 0
+        totalMeasurements = 0
+        lastTimestamp = timestamp
+
+    time.sleep(5)
